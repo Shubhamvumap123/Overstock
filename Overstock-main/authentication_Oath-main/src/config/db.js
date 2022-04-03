ed31a00a229bccfba5df0f29f3b29f87b4bd435c
AmaanHaider <97442978+AmaanHaider@users.noreply.github.com>
2022-04-03 18:11:15 +0530

    UPDATE

--- Overstock-main/authentication_Oath-main/src/config/db.js
+++
@@ -1,4 +0,0 @@
-const mongoose =require("mongoose");
-module.exports = () => {
-    return mongoose.connect("mongodb+srv://shubham123:12Shubh34@cluster0.0wzjl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-};
