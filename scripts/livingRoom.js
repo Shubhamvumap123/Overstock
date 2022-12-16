217939fb33383c27c1ba623d78caa9c229b6d5d0
Krunal Borhade <krunalborhade2897@gmail.com>
2022-12-16 17:02:56 +0530

    Link change

--- scripts/livingRoom.js
+++ scripts/livingRoom.js
@@ -14,9 +14,9 @@ let parent1 = document.getElementById("main2")
 appendData(ourSty,parent1)
 //https://overstockapi.herokuapp.com/products/mainCategory=${selected_cat}
 
-//let res = await getData("https://overstockapi.herokuapp.com/products/")
+let res = await getData("https://overstockapi.herokuapp.com/products/")
 
-let res = await getData("http://localhost:5000/products")
+
 
 let cont = document.getElementById("prod-list")
 
